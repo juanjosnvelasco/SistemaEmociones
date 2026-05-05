5580b76f177c885bab5b5e76617def29a8494acf
juanc <juan_josn.velasco@uao.edu.co>
2026-05-05 12:31:22 -0500

    Inicio de creacion del ptoyecto

---
+++ SistemaEmociones/src/main/java/com/mycompany/sistemaemociones/SistemaEmociones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemaemociones;
+
+/**
+ *
+ * @author juanc
+ */
+public class SistemaEmociones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
